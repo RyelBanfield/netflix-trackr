83679746628d8e7e1a74088e8082011920b2ff3b
Ryel Banfield <ryelbanfield@gmail.com>
2021-04-05 09:40:48 -0400

    Updte views

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,5 @@
 class Show < ApplicationRecord
   validates :title, :comment, :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
   belongs_to :user
 end
